c14403ed76b82d4b54abccea687b88b92f0fe15c
florian <florian.aubin@laposte.net>
2022-11-25 16:24:54 +0100

    cardItem + correctif navBar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, BrowserRouter} from "react-router-dom";
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import CardItem from "./components/CardItem/CardItem";
+
 
 function App() {
-  return (
-    <Router>
-      <NavBar />
-      <Routes >
-      </Routes >
-    </Router>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<NavBar/>}>
+                    <Route path="dofus" element={<CardItem/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import CardItem from "./components/CardItem/CardItem";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <div>
+        <App></App>
+    </div>
 );
 
 // If you want your app to work offline and load faster, you can change
