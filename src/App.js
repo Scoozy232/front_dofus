7f67d76f0655735a50d9ab8501184b88348c4329
scoozy23 <scoozy23@gmail.com>
2022-12-01 08:18:21 +0100

    non

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
-import Accueil from './components/Accueil';
+import CardItem from "./components/CardItem/CardItem";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes >
-        <Route path='/' element={NavBar} />
-        <Route index element={Accueil} />
-      </Routes >
-    </BrowserRouter>
+            <Routes>
+                <Route path="/" element={<NavBar/>}>
+                    <Route path="dofus" element={<CardItem/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
   );
 }
 
