8618880a0835f661210c0c5fd08755a8c5ce6d40
florian <florian.aubin@laposte.net>
2022-12-01 17:00:16 +0100

    correction navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
-import CardItem from "./components/CardItem/CardItem";
 import ListMenu from "./components/ItemMenu/ListMenu";
 import Welcome from "./Welcome/Welcome";
 import LoginRegister from './components/LoginRegister/LoginRegister';
@@ -11,18 +10,18 @@ import Login from './components/Login/Login';
 
 function App() {
     return (
-            <BrowserRouter>
-                <NavBar/>
-                <Routes>
-                    <Route path="/" element={<Welcome/>}/>
-                    <Route path="dofus" element={<ListMenu title={"Liste des dofus"}/>}/>
-                    <Route path="donjons" element={<ListMenu title={"Liste des donjons"}/>}/>
-                    <Route path="quetes"element={<ListMenu title={"Liste des quêtes"}/>}/>
-                    <Route path="signup" element={<LoginRegister/>}/>
-                    <Route path="signin" element={<Login/>}/>
-                    <Route path="*" element={<Welcome/>}/> {/*futur 404*/}
-                </Routes>
-            </BrowserRouter>
+        <BrowserRouter>
+            <NavBar/>
+            <Routes>
+                <Route path="/" element={<Welcome/>}/>
+                <Route path="dofus" element={<ListMenu title={"Liste des dofus"}/>}/>
+                <Route path="donjons" element={<ListMenu title={"Liste des donjons"}/>}/>
+                <Route path="quetes" element={<ListMenu title={"Liste des quêtes"}/>}/>
+                <Route path="signup" element={<LoginRegister/>}/>
+                <Route path="signin" element={<Login/>}/>
+                <Route path="*" element={<Welcome/>}/> {/*futur 404*/}
+            </Routes>
+        </BrowserRouter>
     );
 }
 
