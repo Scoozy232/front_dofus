41e279dae66d9a41bfe461f208bd3558a1fbcda7
scoozy23 <scoozy23@gmail.com>
2022-12-02 13:54:07 +0100

    backgorund dynamique

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -15,7 +15,7 @@ function NavBar() {
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
+                    <Nav className="me-auto" >
                         <Nav.Link href="/dofus">
                             <Link className={'text-decoration-none'} to={"/dofus"}>Dofus</Link>
                         </Nav.Link>
