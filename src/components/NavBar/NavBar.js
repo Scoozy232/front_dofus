b01da7aa7e5abb0471a64541a96ef7baa9016258
florian <florian.aubin@laposte.net>
2022-12-02 13:39:12 +0100

    nav

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,6 +4,7 @@ import './NavBar.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import emeraude from '../../assets/dofus/emeraude.png'
 
 
 function NavBar() {
@@ -11,19 +12,25 @@ function NavBar() {
         <Navbar className={'back navbar-dark'} expand="lg">
             <Container>
                 <Navbar.Brand>
-                    <Link className={'text-decoration-none'} to={"/acceuil"}>Accueil</Link>
+                    <Link className={'text-decoration-none'} to={"/acceuil"}>Dofus pour les pros</Link>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link href="/dofus">
-                            <Link className={'text-decoration-none'} to={"/dofus"}>Dofus</Link>
+                            <Link className={'text-decoration-none'} to={"/dofus"}>
+                                <img src={emeraude} alt={'image'}/>
+                            </Link>
                         </Nav.Link>
                         <Nav.Link to="/donjon">
-                            <Link className={'text-decoration-none'} to={"/donjons"}>Donjons</Link>
+                            <Link className={'text-decoration-none'} to={"/donjons"}>
+                                <img src={emeraude} alt={'image'}/>
+                            </Link>
                         </Nav.Link>
                         <Nav.Link href="#link">
-                            <Link className={'text-decoration-none'} to={"/quetes"}>Quêtes</Link>
+                            <Link className={'text-decoration-none'} to={"/quetes"}>
+                                <img src={emeraude} alt={'image'}/>
+                            </Link>
                         </Nav.Link>
                     </Nav>
 
