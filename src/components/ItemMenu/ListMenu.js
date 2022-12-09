e3c769962dfb79023741c2c81cf26817e90a44d7
scoozy23 <scoozy23@gmail.com>
2022-12-09 13:34:26 +0100

    map

--- src/components/ItemMenu/ListMenu.js
+++ src/components/ItemMenu/ListMenu.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from "axios";
 import CardItem from "../CardItem/CardItem";
 import emeraude from '../../assets/dofus/emeraude.png'
 import turquoise from '../../assets/dofus/turquoise.png'
@@ -6,29 +7,48 @@ import vita from '../../assets/carac/vita.png'
 import crit from '../../assets/carac/crit.png'
 import './ListMenu.css'
 
+
+
+
 function ListMenu({title}) {
+
+    const [dofus, setDofus] = React.useState("");
+    const baseURL = "http://localhost:3001/api/dofus";
+
+    React.useEffect(() => {
+        axios.get(baseURL,{
+            headers: {
+                "Access-Control-Allow-Origin" : "*",
+                 "Content-type": "Application/json",
+                Authorization : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MzkxZjVjMTBkMDE4NTk1NjQ3NTk4NTMiLCJpYXQiOjE2NzA1MTAyNDEsImV4cCI6MTY3MDU5NjY0MX0.d5VH8ZHQtgTDW09W3RkHaG05eeS2ehksPbiIzRgwTMo",
+                email : "florian.aubin@laposte.net"
+            }
+        }).then((response) => {
+            const data = response;
+            setDofus(response.data);
+            console.log(response.data)
+        }).catch(err => console.log(err));
+    }, []);
+    
     return (
         <div className={'d-flex flex-column align-items-center'}>
             <h1 className={'text-center'}>{title}</h1>
             <div className="row">
-                <CardItem image={emeraude} title={'Dofus Emeraude'} underTitle={'Donne 100% du bouclier à son porteur'}
-                          level={'Niveau : 100'}
-                          underLevel={<li className="list-group-item"><img src={vita} alt="vitalité"/> 200 vitalité
-                          </li>}
-                          buttonText1={'Comment obtenir ?'}
-                          buttonText2={'Acquis !'}
-                          href1={'/dofus/1'}
-                          href2={'/dofus'}
-                />
-                <CardItem image={turquoise} title={'Dofus Turquoise'} underTitle={'Donne 1% de dommage finaux par crit'}
-                          level={'Niveau : 160'}
-                          underLevel={<li className="list-group-item"><img src={crit} alt="critique"/> 10% critique
-                          </li>}
-                          buttonText1={'Comment obtenir ?'}
-                          buttonText2={'Acquis !'}
-                          href1={'/dofus/2'}
-                          href2={'/dofus'}
-                />
+                {
+                    dofus.map((item, i) => 
+                    <CardItem image={emeraude} title={item.nom} underTitle={item.propriete}
+                    level={item.niveau}
+                    underLevel={<li className="list-group-item"><img src={vita} alt="vitalité"/> {item.stat_native}
+                    </li>}
+                    buttonText1={'Comment obtenir ?'}
+                    buttonText2={'Acquis !'}
+                    href1={'/dofus/1'}
+                    href2={'/dofus'}
+                 />
+                       
+                    )
+                }
+                 
             </div>
         </div>
     );
