4077c4df595729b13fc0c9930c08e97934da2749
florian <florian.aubin@laposte.net>
2023-01-13 11:07:55 +0100

    fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 function Footer() {
     return (
         <footer className="mt-auto w-full mx-auto my-auto bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-900 dark:border-gray-600">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="/front_dofus/" className="hover:underline">DofusPourLesPro</a>. All Rights Reserved.
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="/front_dofus/" className="hover:underline">DofusPourLesPros</a>. All Rights Reserved.
             </span>
             <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
                 <li>
--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -5,8 +5,8 @@ function Welcome() {
 
     return (
         <div className='min-h-full relative h-full pb-10 text-white'>
-            <h1 className={'text-center text-3xl font-bold underline '}>Bienvenue sur Dofus pour les pro !</h1>
-            <div className="w-full p-6 m-auto my-10  bg-gray-900 rounded-lg   lg:max-w-xl">
+            <h1 className={'text-center text-4xl font-bold text-gray-900 pt-10'}>Bienvenue sur Dofus pour les pros !</h1>
+            <div className="w-full p-6 m-auto my-10  bg-gray-900 rounded-lg lg:max-w-xl">
                 <div><p>Cette application web à été créé pour toi, joueur Dofus, afin de t'aider à savoir où tu en es rendus sur le jeu.</p>
                 <div><p>Tu vas pouvoir savoir : </p>
                 <ul className='list-inside list-disc'>
